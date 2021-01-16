6e25de8d1259213e23b9c333288fb8adcf7a3f6b
MichalOleszak <oleszak.michal@gmail.com>
2021-01-16 17:38:17 +0100

    Update been

---
+++ been/generate_html_map.py
@@ -0,0 +1,11 @@
+import plotly.express as px
+import pandas as pd
+
+been = pd.read_csv("been.csv")
+
+fig = px.scatter_mapbox(been, lat="lat", lon="lon", hover_name="place",
+                        color_discrete_sequence=["darkorange"], zoom=2)
+fig.update_layout(mapbox_style="carto-positron")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+fig.write_html("been.html")
