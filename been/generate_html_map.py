44608d433ca5fbf8b35b5aceb6bbce18c94cd0d6
MichalOleszak <oleszak.michal@gmail.com>
2022-01-18 23:32:30 +0100

    Update map generation

--- been/generate_html_map.py
+++ been/generate_html_map.py
@@ -4,6 +4,7 @@ import pandas as pd
 been = pd.read_csv("been.csv")
 
 fig = px.scatter_mapbox(been, lat="lat", lon="lon", hover_name="place",
+                        hover_data={"place": False, "lat": False, "lon": False},
                         color_discrete_sequence=["darkorange"], zoom=2)
 fig.update_layout(mapbox_style="carto-positron")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
