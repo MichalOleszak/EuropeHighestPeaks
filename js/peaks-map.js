76840b0f332510dd88c063e9a76088acfdd08c09
MichalOleszak <oleszak.michal@gmail.com>
2026-09-18 19:17:30 +0200

    Replace Carto tiles with OpenStreetMap

--- js/peaks-map.js
+++ js/peaks-map.js
@@ -29,13 +29,10 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   // initialize map with fractional zoom support – will recenter when data loads
   var map = L.map('map', { zoomSnap: 0.5, zoomDelta: 0.25 });
-  // use a simpler, light-styled basemap with minimal administrative borders
-  var cartoKey = window.CARTO_BASEMAPS_KEY;
-  var tileUrl = 'https://basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{r}.png' +
-    (cartoKey ? '?key=' + encodeURIComponent(cartoKey) : '');
-  L.tileLayer(tileUrl, {
+  // OpenStreetMap tiles are free to use with attribution and need no API key.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 13,
-    attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   map.invalidateSize();
 
